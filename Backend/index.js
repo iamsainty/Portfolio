5586673fb26d3809602dd8358de4c6a6c0d956bf
Priyanshu Chaurasiya <sainty@Priyanshus-MacBook-Air.local>
2024-05-19 08:18:02 +0530

    used body parser and few improvements

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const connectToMongo= require('./db');
 const express = require('express');
 const cors = require('cors'); // Import the cors middleware
+const bodyParser = require("body-parser");
 connectToMongo();
 
 const app = express();
@@ -8,6 +9,7 @@ const port = 5002;
 
 app.use(express.json()); // Middleware for parsing JSON bodies
 app.use(cors()); // Add the cors middleware to allow all origins
+app.use(bodyParser.json());
 
 app.use('/auth', require('./routes/auth'));
 app.use('/blog', require('./routes/blog'));
