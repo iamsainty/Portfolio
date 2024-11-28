643cfa119d94fe56272d2f95918e42a392e05e22
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2024-11-28 23:19:16 +0530

    authentication modal setup

--- Frontend/src/components/AuthModal/AuthModal.js
+++ Frontend/src/components/AuthModal/AuthModal.js
@@ -1,7 +1,9 @@
-import React, { useRef, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 // import { FcGoogle } from "react-icons/fc";
 import styled from "styled-components";
+import userAuthContext from "../context/userAuth/userAuthContext";
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
 const Content = styled.div`
   height: 100vh;
@@ -32,14 +34,28 @@ const ModalHeading = styled.h2`
   font-weight: bold;
 `;
 
+const Container = styled.div`
+  height: 100vh;
+  margin: 0;
+`;
+
+const CloseButton = styled.div`
+  margin: 0;
+  height: 30vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const FormContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: end;
   gap: 10px;
   width: 25vw;
-  height: 100vh;
+  height: 70vh;
+  margin: 0;
 
   @media (max-width: 768px) {
     width: 80vw;
@@ -56,6 +72,11 @@ const Input = styled.input`
   &:focus {
     border: 1px solid #333;
   }
+`;
+
+const ErrorText = styled.p`
+  color: red;
+  font-size: 14px;
 `;
 
 const SubmitButton = styled(Button)`
@@ -156,28 +177,33 @@ const OTPInput = styled.input`
 function AuthModal({ show, closeModal }) {
   const [currentPage, setCurrentPage] = useState("signIn");
 
+  const { signin, signup, sendOtp, otpSent, error } =
+    useContext(userAuthContext);
+
   const [name, setName] = useState("");
-  const [email, setEmail] = useState("sainty@gmail.com");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const [err, setErr] = useState(null);
 
   const openSignIn = () => {
     setCurrentPage("signIn");
+    setErr(null);
   };
 
   const openSignUp = () => {
     setCurrentPage("signUp");
+    setErr(null);
   };
 
   const [otp, setOtp] = useState(["", "", "", ""]);
   const inputs = useRef([]);
 
   const handleChange = (value, index) => {
-    // Update the current OTP digit
     const newOtp = [...otp];
     newOtp[index] = value;
     setOtp(newOtp);
 
-    // Move focus to the next input if the value is a single digit
     if (value && index < inputs.current.length - 1) {
       inputs.current[index + 1].focus();
     }
@@ -206,39 +232,55 @@ function AuthModal({ show, closeModal }) {
     inputs.current[lastIndex].focus();
   };
 
+  const handleSignIn = async () => {
+    await signin(email, password);
+    if (error === null) {
+      closeModal();
+    }
+  };
 
-  const handleSignIn = () => {
+  const handleSignUp = async () => {
+    await sendOtp(email);
+    setErr(error);
+    if (err === null) {
+      setCurrentPage("otp");
+    }
+  };
 
-  }
-
-  const handleSignUp = () => {
-    setCurrentPage('otp');
-  }
-
-  const handleOtpVerification = () => {
-
-  }
+  const handleOtpVerification = async () => {
+    if (otpSent && otp.join("") === otpSent.toString()) {
+      await signup(name, email, password);
+      setErr(error);
+      if (err === null) {
+        closeModal();
+      }
+    } else {
+      setErr("Invalid OTP, Try again");
+    }
+  };
 
   const renderContent = () => {
     switch (currentPage) {
       case "signIn":
         return (
-          <FormContainer>
-            <ModalHeading>Sign In</ModalHeading>
-            <Input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <Input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <SubmitButton onClick={handleSignIn}>Sign In</SubmitButton>
-            {/* <DividerContainer>
+          <Container>
+            <FormContainer>
+              <ModalHeading>Sign In</ModalHeading>
+              <Input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {err && <ErrorText>{err}</ErrorText>}
+              <SubmitButton onClick={handleSignIn}>Sign In</SubmitButton>
+              {/* <DividerContainer>
               <DividerLine />
               <DividerText>Or sign in with</DividerText>
               <DividerLine />
@@ -247,72 +289,88 @@ function AuthModal({ show, closeModal }) {
               <FcGoogle />
               Sign in with Google
             </GoogleButton> */}
-            <SwitchPage onClick={openSignUp}>
-              New here ? Register now
-            </SwitchPage>
-          </FormContainer>
+              <SwitchPage onClick={openSignUp}>
+                New here ? Register now
+              </SwitchPage>
+            </FormContainer>
+            <CloseButton>
+              <IoIosCloseCircleOutline size={50} />
+            </CloseButton>
+          </Container>
         );
       case "signUp":
         return (
-          <FormContainer>
-            <ModalHeading>Sign Up</ModalHeading>
-            <Input
-              type="text"
-              placeholder="Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <Input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <Input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <SubmitButton onClick={handleSignUp}>Sign Up</SubmitButton>
-            {/* <DividerContainer>
+          <Container>
+            <FormContainer>
+              <ModalHeading>Sign Up</ModalHeading>
+              <Input
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <Input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {err && <ErrorText>{err}</ErrorText>}
+              <SubmitButton onClick={handleSignUp}>Sign Up</SubmitButton>
+              {/* <DividerContainer>
               <DividerLine />
               <DividerText>Or sign up with</DividerText>
               <DividerLine />
-            </DividerContainer>
-            <GoogleButton>
+              </DividerContainer>
+              <GoogleButton>
               <FcGoogle />
               Sign up with Google
-            </GoogleButton> */}
-            <SwitchPage onClick={openSignIn}>
-              Already Registered ? Login Instead
-            </SwitchPage>
-          </FormContainer>
+              </GoogleButton> */}
+              <SwitchPage onClick={openSignIn}>
+                Already Registered ? Login Instead
+              </SwitchPage>
+            </FormContainer>
+            <CloseButton>
+              <IoIosCloseCircleOutline size={50} />
+            </CloseButton>
+          </Container>
         );
       case "otp":
         return (
-          <FormContainer>
-            <ModalHeading>Verify your email</ModalHeading>
-            <OtpText>
-              {" "}
-              An OTP has been sent to {email}, verify to proceed
-            </OtpText>
-            <OTPContainer onPaste={handlePaste}>
-              {otp.map((digit, index) => (
-                <OTPInput
-                  key={index}
-                  type="text"
-                  maxLength="1"
-                  value={digit}
-                  onChange={(e) => handleChange(e.target.value, index)}
-                  onKeyDown={(e) => handleKeyDown(e, index)}
-                  ref={(el) => (inputs.current[index] = el)} // Assign refs dynamically
-                />
-              ))}
-            </OTPContainer>
-
-            <SubmitButton onClick={handleOtpVerification}>Verify</SubmitButton>
-          </FormContainer>
+          <Container>
+            <FormContainer style={{height : '90vh', justifyContent : 'center'}}>
+              <ModalHeading>Verify your email</ModalHeading>
+              <OtpText>
+                {" "}
+                An OTP has been sent to {email}, verify to proceed
+              </OtpText>
+              <OTPContainer onPaste={handlePaste}>
+                {otp.map((digit, index) => (
+                  <OTPInput
+                    key={index}
+                    type="text"
+                    maxLength="1"
+                    value={digit}
+                    onChange={(e) => handleChange(e.target.value, index)}
+                    onKeyDown={(e) => handleKeyDown(e, index)}
+                    ref={(el) => (inputs.current[index] = el)} // Assign refs dynamically
+                  />
+                ))}
+              </OTPContainer>
+              {err && <ErrorText>{err}</ErrorText>}
+              <SubmitButton onClick={handleOtpVerification}>
+                Verify
+              </SubmitButton>
+            </FormContainer>
+            <CloseButton>
+            </CloseButton>
+          </Container>
         );
       default:
         return null;
