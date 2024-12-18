cc43d8a28191eadbe2858bdb0f83e2946de47bc9
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2024-12-18 21:34:24 +0530

    added side navigation

--- Frontend/src/components/User/ManageProfile/ManageProfile.js
+++ Frontend/src/components/User/ManageProfile/ManageProfile.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
+import SideNavigation from "./SideNavigation";
+import MyProfile from "./MyProfile";
+import EditProfile from "./EditProfile";
+import ChangePassword from "./ChangePassword";
+import NotificationSetting from "./NotificationSetting";
+import AccountSetting from "./AccountSetting";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  margin-top: 50px;
+  width: 80vw;
+  height: 75vh;
+  border: 1px solid #aaa;
+  border-radius: 15px;
+`;
+
+const NavigationContainer = styled.div`
+  width: 25%;
+  height: 100%;
+`;
+
+const ContentContainer = styled.div`
+  width: 75%;
+  height: 100%;
+`;
 
 function ManageProfile() {
+  const [activeSection, setActiveSection] = useState("My Profile");
+
   return (
-    <div>ManageProfile</div>
-  )
+    <Container className="container d-flex">
+      <NavigationContainer>
+        <SideNavigation
+          activeSection={activeSection}
+          setActiveSection={setActiveSection}
+        />
+      </NavigationContainer>
+      <ContentContainer>
+        {activeSection === "My Profile" && <MyProfile />}
+        {activeSection === "Edit Profile" && <EditProfile />}
+        {activeSection === "Change Password" && <ChangePassword />}
+        {activeSection === "Notification Setting" && <NotificationSetting />}
+        {activeSection === "Account Setting" && <AccountSetting />}
+      </ContentContainer>
+    </Container>
+  );
 }
 
-export default ManageProfile
+export default ManageProfile;
--- Frontend/src/components/User/ManageProfile/SideNavigation.js
+++ Frontend/src/components/User/ManageProfile/SideNavigation.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { FaUserAlt, FaUserEdit, FaLock, FaBell, FaCog } from "react-icons/fa";
 
-function SideNavigation() {
+const navItems = [
+  { name: "My Profile", icon: <FaUserAlt /> },
+  { name: "Edit Profile", icon: <FaUserEdit /> },
+  { name: "Change Password", icon: <FaLock /> },
+  { name: "Notification Settings", icon: <FaBell /> },
+  { name: "Account Settings", icon: <FaCog /> },
+];
+
+const Container = styled.div`
+  height: 100%;
+  padding: 10px;
+  border-right: 1px solid #aaa;
+`;
+
+const NavigationList = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+`;
+
+const NavigationItem = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  padding: 12px 20px;
+  border-radius: 10px;
+  gap: 10px;
+  color: #555;
+  transition: background-color 0.3s ease, transform 0.2s ease;
+
+  &:hover {
+    background-color: #f5f5f5;
+    transform: scale(1.05); 
+  }
+
+  &.active {
+    background-color: #f0f0f0;
+    font-weight: bold;
+  }
+`;
+
+function SideNavigation({ activeSection, setActiveSection }) {
   return (
-    <div>SideNavigation</div>
-  )
+    <Container>
+      <NavigationList>
+        {navItems.map((item) => (
+          <NavigationItem
+            key={item.name}
+            onClick={() => setActiveSection(item.name)}
+            className={activeSection === item.name ? "active" : ""}
+          >
+            {item.icon}
+            {item.name}
+          </NavigationItem>
+        ))}
+      </NavigationList>
+    </Container>
+  );
 }
 
-export default SideNavigation
+export default SideNavigation;
