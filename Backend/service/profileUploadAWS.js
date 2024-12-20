f757acc5325b7efdce4ef179c464c7abe9ab7744
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2024-12-20 22:15:51 +0530

    fix unavailable enviornment variable name

--- Backend/service/profileUploadAWS.js
+++ Backend/service/profileUploadAWS.js
@@ -6,7 +6,7 @@ const path = require("path");
 dotenv.config();
 
 const awsS3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
 });
