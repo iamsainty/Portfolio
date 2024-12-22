82a85316bfdc3f6d15ad5c24f17ec893933c6e9c
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2024-12-22 12:05:57 +0530

    UI update of my profile section

--- Frontend/src/components/User/ManageProfile/MyProfile.js
+++ Frontend/src/components/User/ManageProfile/MyProfile.js
@@ -1,60 +1,105 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 import userContext from "../../context/user/userContext";
+import Loading from "../../Loading";
 
 const Container = styled.div`
   padding: 20px;
   width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
-const Heading = styled.div`
-  font-size: 24px;
-  font-weight: bold;
-  text-align: center;
-  margin-bottom: 30px;
-`;
-
-const ProfileWrapper = styled.div`
+const ContentWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   width: 100%;
-  height: 300px;
+  height: 100%;
   justify-content: center;
   align-items: center;
-  gap: 30px;
-  margin-top: 30px;
+  gap: 50px;
+  padding : 0 0 75px 0;
 `;
 
-const ProfileImage = styled.img`
-  width: 200px;
-  height: 200px;
-  border-radius: 50%;
-  object-fit: cover;
-  border: 3px solid #ddd;
+const Greeting = styled.h1`
+  font-size: 25px;
+  font-weight: 500;
+  color: #444;
 `;
 
 const UserInfo = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
-  gap: 15px;
+  align-items: center;
+  gap: 50px;
 `;
 
-const UserDetail = styled.p`
-  font-size: 18px;
+const ProfileWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid #444;
+  border-radius: 50%;
+  box-shadow: 0 0 25px 0 rgba(0, 0, 0, 0.1);
+  padding: 2.5px;
+`;
+
+const ProfileImage = styled.img`
+  width: 175px;
+  height: 175px;
+  border-radius: 50%;
+  object-fit: cover;
+  box-shadow: 0 0 25px 0 rgba(0, 0, 0, 0.1);
+`;
+
+const UserDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
   margin: 5px 0;
   color: #333;
 `;
 
+const Detail = styled.div`
+  display: flex;
+  align-items: center;
+  color: #444;
+  gap: 10px;
+  border: 1px solid #444;
+  border-radius: 10px;
+  padding-right: 10px;
+  font-weight: 500;
+
+`;
+
+const FieldName = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5px 10px;
+  background-color: #444;
+  color: #fff;
+  border-radius: 5px;
+  width: 75px;
+`;
+
+
 function MyProfile() {
   const { user, fetchUserDetails } = useContext(userContext);
+
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchDetails = async () => {
       try {
+        setLoading(true);
         await fetchUserDetails();
+        if(user.profilePictureUrl === null){
+          user.profilePictureUrl = 'https://hey-sainty.s3.ap-south-1.amazonaws.com/profile-pictures/Default+Profile+Picture+-+Hey+Sainty.png';
+        }
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching user details:", error);
       }
@@ -64,21 +109,50 @@ function MyProfile() {
     // eslint-disable-next-line
   }, []);
 
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <Loading />
+      </div>
+    );
+  }
+
   return (
     <Container>
-      <Heading>My Profile</Heading>
       {user && (
-        <ProfileWrapper>
-          <ProfileImage src={user.profilePictureUrl} alt="Profile" />
+        <ContentWrapper>
+          <Greeting>
+            Hey
+            <span
+              style={{
+                fontFamily: "'Cedarville Cursive', cursive",
+                fontSize: "50px",
+                fontWeight: "bold",
+                padding: "0 10px",
+              }}
+            >
+              {user.name.split(" ")[0]}
+            </span>
+             what's up?
+          </Greeting>
           <UserInfo>
-          <UserDetail>
-            <strong>Name:</strong> {user.name}
-          </UserDetail>
-          <UserDetail>
-            <strong>Email:</strong> {user.email}
-          </UserDetail>
+            <ProfileWrapper>
+              <ProfileImage src={user.profilePictureUrl} alt="Profile" />
+            </ProfileWrapper>
+            <UserDetail>
+              <Detail><FieldName>Name</FieldName> {user.name}</Detail>
+              <Detail><FieldName>Email</FieldName> {user.email}</Detail>
+            </UserDetail>
           </UserInfo>
-        </ProfileWrapper>
+        </ContentWrapper>
       )}
     </Container>
   );
