9ac2effa61185ad21c64cb89720de936318e8608
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2025-02-02 17:26:33 +0530

    mode toggle

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,14 @@
 import React from "react";
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import {
   Sheet,
   SheetContent,
   SheetDescription,
-  SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
@@ -21,6 +17,7 @@ import { RxEnter } from "react-icons/rx";
 
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { ModeToggle } from "./ModeToggleButton";
 
 const NavbarLinks = [
   { label: "Home", href: "/" },
@@ -29,64 +26,10 @@ const NavbarLinks = [
   { label: "Blog", href: "/blog" },
 ];
 
-const Pages = [
-  {
-    label: "About",
-    href: "/about",
-    description: "Learn more about Hey Sainty",
-  },
-  {
-    label: "Contact",
-    href: "/contact",
-    description: "Reach out for inquiries",
-  },
-  {
-    label: "Disclaimer",
-    href: "/disclaimer",
-    description: "Read our website disclaimer",
-  },
-  {
-    label: "Privacy Policy",
-    href: "/privacy-policy",
-    description: "Understand our privacy policy",
-  },
-  {
-    label: "GDPR Compliance",
-    href: "/gdpr-compliance",
-    description: "Check our GDPR compliance",
-  },
-];
-
 const Navbar = ({ user }) => {
-  const AccountOptions = user
-    ? [
-        {
-          label: "Manage Profile",
-          href: "/profile",
-          description: "Go to your profile",
-        },
-        {
-          label: "Sign Out",
-          href: "/logout",
-          description: "Log out from your account",
-        },
-      ]
-    : [
-        {
-          label: "Sign In",
-          href: "/login",
-          description: "Already have an account? Sign in",
-        },
-        {
-          label: "Sign Up",
-          href: "/register",
-          description: "New here? Create an account",
-        },
-      ];
-
   return (
     <header className="flex justify-center w-full">
-      <nav className="fixed top-8 w-4/5 sm:w-3/5 container border border-slate-400 px-5 sm:pl-10 sm:pr-2 py-2 rounded-full flex justify-between items-center">
+      <nav className="fixed top-8 w-4/5 sm:w-3/5 container border border-black dark:border-white px-5 sm:pl-10 sm:pr-2 py-1 sm:py-2 rounded-full flex justify-between items-center">
         {/* Logo / Brand */}
         <Link href="/" aria-label="Homepage" title="Hey Sainty - Home">
           <span className="text-md sm:text-lg font-semibold">Hey Sainty</span>
@@ -97,7 +40,7 @@ const Navbar = ({ user }) => {
           <NavigationMenuList className="flex items-center justify-between gap-7">
             {NavbarLinks.map((link) => (
               <NavigationMenuItem key={link.href}>
-                <Link href={link.href} legacyBehavior passHref>
+                <Link href={link.href} passHref>
                   <NavigationMenuLink className="text-md rounded-md cursor-pointer">
                     {link.label}
                   </NavigationMenuLink>
@@ -106,46 +49,50 @@ const Navbar = ({ user }) => {
             ))}
 
             {/* Account Dropdown */}
-            <NavigationMenuItem>
+            {/* <NavigationMenuItem>
               <Button className="text-md rounded-full px-5 py-2">
                 Get In <RxEnter />
               </Button>
-            </NavigationMenuItem>
+            </NavigationMenuItem> */}
           </NavigationMenuList>
         </NavigationMenu>
 
-        <Sheet key={"left"}>
-          <SheetTrigger asChild className="lg:hidden">
-            <RxHamburgerMenu />
-          </SheetTrigger>
-          <SheetContent
-            side={"right"}
-            className="flex flex-col w-4/5 justify-start items-center gap-10 pt-24"
-          >
-            <SheetTitle className="text-2xl">Hey Sainty</SheetTitle>
-            <SheetDescription>
-              <NavigationMenu>
-                <NavigationMenuList className="flex flex-col items-center justify-evenly gap-3">
-                  {NavbarLinks.map((link) => (
-                    <NavigationMenuItem key={link.href}>
-                      <Link href={link.href} legacyBehavior passHref>
-                        <NavigationMenuLink className="text-lg rounded-md cursor-pointer">
-                          {link.label}
-                        </NavigationMenuLink>
-                      </Link>
-                    </NavigationMenuItem>
-                  ))}
+        <div className="flex items-center justify-between gap-3">
+          <ModeToggle />
 
-                  <NavigationMenuItem>
-                    <Button className="text-md rounded-full px-5 py-2">
-                      Get In <RxEnter />
-                    </Button>
-                  </NavigationMenuItem>
-                </NavigationMenuList>
-              </NavigationMenu>
-            </SheetDescription>
-          </SheetContent>
-        </Sheet>
+          <Sheet>
+            <SheetTrigger asChild className="lg:hidden">
+              <RxHamburgerMenu />
+            </SheetTrigger>
+            <SheetContent
+              side={"right"}
+              className="flex flex-col w-4/5 justify-start items-center gap-10 pt-24"
+            >
+              <SheetTitle className="text-2xl">Hey Sainty</SheetTitle>
+              <SheetDescription>
+                <NavigationMenu>
+                  <NavigationMenuList className="flex flex-col items-center justify-evenly gap-3">
+                    {NavbarLinks.map((link) => (
+                      <NavigationMenuItem key={link.href}>
+                        <Link href={link.href} legacyBehavior passHref>
+                          <NavigationMenuLink className="text-lg rounded-md cursor-pointer">
+                            {link.label}
+                          </NavigationMenuLink>
+                        </Link>
+                      </NavigationMenuItem>
+                    ))}
+
+                    {/* <NavigationMenuItem>
+                      <Button className="text-md rounded-full px-5 py-2">
+                        Get In <RxEnter />
+                      </Button>
+                    </NavigationMenuItem> */}
+                  </NavigationMenuList>
+                </NavigationMenu>
+              </SheetDescription>
+            </SheetContent>
+          </Sheet>
+        </div>
       </nav>
     </header>
   );
