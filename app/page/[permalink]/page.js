7b9314b364a7a79fb921a0886c4f9ee84872739d
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2025-06-21 13:28:08 +0530

    seo metadata for pages

--- app/page/[permalink]/page.js
+++ app/page/[permalink]/page.js
@@ -27,6 +27,79 @@ async function getPage(permalink) {
   }
 }
 
+export async function generateMetadata({ params }) {
+  const page = await getPage(params.permalink);
+
+  if (!page) {
+    return notFound();
+  }
+
+  const pageUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/page/${page.permalink}`;
+
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    url: pageUrl,
+    headline: page.title,
+    description: page.description,
+    image: [
+      {
+        url: page.coverimage,
+        width: 1200,
+        height: 630,
+        alt: page.title + " - Hey Sainty",
+      },
+    ],
+    datePublished: page.dateCreated,
+    dateModified: page.lastUpdated,
+  };
+
+  return {
+    title: page.title + " - Hey Sainty",
+    description: page.description,
+    openGraph: {
+      title: page.title + " - Hey Sainty",
+      description: page.description,
+      url: pageUrl,
+      images: [
+        {
+          url: page.coverimage,
+          width: 1200,
+          height: 630,
+          alt: page.title,
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+      siteName: "Hey Sainty",
+      url: pageUrl,
+    },
+    alternates: {
+      canonical: pageUrl,
+    },
+    robots: {
+      index: true,
+      follow: true,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: page.title + " - Hey Sainty",
+      description: page.description,
+      images: [
+        {
+          url: page.coverimage,
+          width: 1200,
+          height: 630,
+          alt: page.title,
+        },
+      ],
+      creator: "@iam__sainty",
+    },
+    jsonLd: JSON.stringify(jsonLd),
+    category: "website",
+  };
+}
+
 export default async function Page({ params }) {
   const page = await getPage(params.permalink);
 
